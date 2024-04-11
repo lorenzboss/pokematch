9aecb61cfe02462adaff44f8f12f159a44dae228
lorenzboss <lorenz.boss@bbzbl-it.ch>
2024-04-11 17:25:53 +0000

    Changed the .env variable to a hardcoded value

--- server/connection.js
+++ server/connection.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
     try {
-        await mongoose.connect(process.env.DB_URL);
+        await mongoose.connect("mongodb://db:27017/pokeDB");
         console.log("Connected to the database");
     } catch (error) {
         console.log(error);
