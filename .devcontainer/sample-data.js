f0eabf580b3ae9f331242cad8b7e959101b37725
lorenzboss <lorenz.boss@bbzbl-it.ch>
2024-04-12 16:29:06 +0000

    initialize sample data automatically

---
+++ .devcontainer/sample-data.js
@@ -0,0 +1,25 @@
+const { connectDB, disconnectDB } = require("../server/connection");
+const winnerUser = require("../server/database/schemas/winnerSchema");
+
+const initializeSampleData = async (data) => {
+  try {
+    await connectDB();
+
+    for (const winUser of data) {
+      await winUser.save();
+    }
+  } catch (error) {
+    console.error("Error initializing sample data:", error);
+  } finally {
+    await disconnectDB();
+    console.log("Sample data initialized successfully!");
+  }
+};
+
+const sampleData = [
+  new winnerUser({ time: 34, moves: 15, user: "Lorenz Boss" }),
+  new winnerUser({ time: 35, moves: 13, user: "Leandro Äebi" }),
+  new winnerUser({ time: 47, moves: 19, user: "Cem Akkaya" }),
+];
+
+initializeSampleData(sampleData);
